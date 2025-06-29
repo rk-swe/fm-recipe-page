b9af861b6701fcce31ba3d3e7c938d1923e08f44
Rohit Krishnamoorthy <rk-swe@users.noreply.github.com>
2025-06-29 13:43:25 +0530

    halfway through styling

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "young-serif": ['"Young Serif"', "serif"],
+        outfit: ['"Outfit"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
